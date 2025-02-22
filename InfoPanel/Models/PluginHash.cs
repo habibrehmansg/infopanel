58f044242afb1cfaa0381a1dbfa59a822d293a60
micah686 <micah686@github.com>
2025-02-22 01:36:29 -0800

    Added plugin state management (WIP)

---
+++ InfoPanel/Models/PluginHash.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace InfoPanel.Models
+{
+    public class PluginHash
+    {
+        public string PluginName { get; set; } = string.Empty;
+        public bool Activated { get; set; } = false;
+        public Dictionary<string, string> Hashes { get; set; } = [];
+
+    }
+}
--- InfoPanel/ViewModels/PluginsViewModel.cs
+++ InfoPanel/ViewModels/PluginsViewModel.cs
@@ -1,20 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using InfoPanel.Models;
 using InfoPanel.Monitors;
+using InfoPanel.Utils;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 
 namespace InfoPanel.ViewModels
 {
-    public class PluginsViewModel: ObservableObject
+    public partial class PluginsViewModel: ObservableObject
     {
         public string PluginsFolder { get; }
         public ObservableCollection<PluginViewModel> Plugins { get; set; } = [];
+        public ObservableCollection<PluginViewModel> AvailablePlugins { get; set; } = [];
 
         public PluginsViewModel() {
 
-            PluginsFolder = Path.Combine(Directory.GetCurrentDirectory(), "plugins");
+            PluginsFolder = PluginStateHelper.PluginsFolder;
 
            foreach(var wrapper in PluginMonitor.Instance._loadedPlugins.Values)
             {
@@ -26,6 +31,42 @@ namespace InfoPanel.ViewModels
                     ConfigFilePath = wrapper.ConfigFilePath
                 });
             }
+
+            var pluginStateList = PluginStateHelper.DecryptAndLoadStateList();
+            foreach (var directory in Directory.GetDirectories(PluginsFolder))
+            {
+                var ph = pluginStateList.FirstOrDefault(x => x.PluginName == Path.GetFileName(directory));
+                if (ph != null && ph.Activated == false)
+                {
+                    AvailablePlugins.Add(new PluginViewModel
+                    {
+                        Name = ph.PluginName,
+                        FileName = string.Empty,
+                        Description = string.Empty,
+                        ConfigFilePath = string.Empty
+                    });
+                }
+            }
+        }
+
+        [RelayCommand]
+        public void SavePluginStates()
+        {
+            var allPlugins = new List<PluginViewModel>();
+            allPlugins.AddRange(Plugins);
+            allPlugins.AddRange(AvailablePlugins);
+            var localPluginList = PluginStateHelper.GetLocalPluginDllHashes();
+
+            foreach (var localPlugin in localPluginList)
+            {
+                var pluginModel = allPlugins.FirstOrDefault(x => x.Name == localPlugin.PluginName);
+                if(pluginModel != null)
+                {
+                    localPlugin.Activated = pluginModel.Activated;
+                    PluginStateHelper.SetPluginState(localPlugin);
+                }
+            }
+            
         }
     }
 
@@ -35,5 +76,6 @@ namespace InfoPanel.ViewModels
         public required string Name { get; set; }
         public required string Description { get; set; }
         public required string? ConfigFilePath { get; set; }
+        public bool Activated { get; set; } = false;
     }
 }
