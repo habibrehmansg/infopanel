8806367a5a30b2fdc04c39bb2cfaaa5f2b14f562
micah686 <micah686@github.com>
2025-03-14 22:36:20 -0700

    Added SerialReader

---
+++ CommunityPlugins/InfoPanel.SerialReader/Port.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace InfoPanel.SerialReader
+{
+    public class Port
+    {
+        public string COMPort { get; set; } = "COM1";
+        public int BaudRate { get; set; }
+        public Parity Parity { get; set; }
+        public int DataBits { get; set; }
+        public StopBits StopBits { get; set; }
+    }
+}
---
+++ CommunityPlugins/InfoPanel.SerialReader/SerialReaderPlugin.cs
@@ -0,0 +1,61 @@
+﻿using InfoPanel.Plugins;
+using System.IO.Ports;
+using System.Text.Json;
+
+namespace InfoPanel.SerialReader
+{
+    internal class SerialReaderPlugin : BasePlugin
+    {
+        public SerialReaderPlugin() : base("serial-reader-plugin", "Serial Reader", "Reads data from serial")
+        {
+        }
+
+        public override string? ConfigFilePath => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "InfoPanel", "plugins", "InfoPanel.SerialReader", "port.json");
+
+        public override TimeSpan UpdateInterval => TimeSpan.FromMilliseconds(500);
+        private SerialPort _serialPort;
+        private readonly PluginSensor _serialSensor = new("SerialData", 0.00f);
+
+        public override void Close()
+        {
+            _serialPort.Dispose();
+        }
+
+        public override void Initialize()
+        {
+            if (!File.Exists(ConfigFilePath))
+            {
+                var portData = new Port();
+                var json = JsonSerializer.Serialize(portData);
+                File.WriteAllText(ConfigFilePath, json);
+            }
+            var config = File.ReadAllText(ConfigFilePath);
+            Port port = JsonSerializer.Deserialize<Port>(config);
+            _serialPort = new SerialPort(port.COMPort, port.BaudRate, port.Parity, port.DataBits, port.StopBits) { DtrEnable = true };
+            _serialPort.Open();
+        }
+
+        public override void Load(List<IPluginContainer> containers)
+        {
+            var container = new PluginContainer("Serial");
+            container.Entries.Add(_serialSensor);
+            containers.Add(container);
+
+            
+        }
+
+        public override void Update()
+        {
+            var message = _serialPort.ReadLine();
+            _serialSensor.Value = (float)Convert.ToDouble(message);
+        }
+
+        public override Task UpdateAsync(CancellationToken cancellationToken)
+        {
+            Update();
+            return Task.CompletedTask;
+        }
+
+        
+    }
+}
