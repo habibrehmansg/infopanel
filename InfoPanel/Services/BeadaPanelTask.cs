6b6dbfa989e9119563a7f9a22ac65e3f80cbe96a
Habib Rehman <13956188+habibrehmansg@users.noreply.github.com>
2025-05-13 22:45:21 +0800

    Add Turing 8.8" REV 1.1 support
    - Migrate WinUSBNet to LibUsbDotNet
    - Beadapanel & TuringPanel separate render & display thread

--- InfoPanel/Services/BeadaPanelTask.cs
+++ InfoPanel/Services/BeadaPanelTask.cs
@@ -4,18 +4,15 @@ using InfoPanel.BeadaPanel.StatusLink;
 using InfoPanel.Extensions;
 using InfoPanel.Models;
 using InfoPanel.Utils;
-using MadWizard.WinUSBNet;
+using LibUsbDotNet;
+using LibUsbDotNet.Main;
 using System;
-using System.Buffers.Binary;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Media.Media3D;
 
 namespace InfoPanel
 {
@@ -76,27 +73,58 @@ namespace InfoPanel
             await Task.Delay(300, token);
             try
             {
-                using var device = USBDevice.GetSingleDevice("{8E41214B-6785-4CFE-B992-037D68949A14}");
-                if (device is null)
+                int vendorId = 0x4e58;
+                int productId = 0x1001;
+
+                var devices = UsbDevice.AllDevices;
+
+                UsbDeviceFinder finder = new(vendorId, productId);
+                using UsbDevice usbDevice = UsbDevice.OpenUsbDevice(finder);
+
+                if (usbDevice == null)
                 {
                     Trace.WriteLine("USB Device not found.");
                     return;
                 }
 
-                Trace.WriteLine($"USB Device Found - {device.Descriptor.FullName}");
 
-                var iface = device.Interfaces.First();
+
+                //using var device = USBDevice.GetSingleDevice("{8E41214B-6785-4CFE-B992-037D68949A14}");
+                //if (device is null)
+                //{
+                //    Trace.WriteLine("USB Device not found.");
+                //    return;
+                //}
+
+                //Trace.WriteLine($"USB Device Found - {device.Descriptor.FullName}");
+
+                //var iface = device.Interfaces.First();
+
+                IUsbDevice? wholeUsbDevice = usbDevice as IUsbDevice;
+                if (wholeUsbDevice != null)
+                {
+                    wholeUsbDevice.SetConfiguration(1);
+                    wholeUsbDevice.ClaimInterface(0);
+                }
+
 
                 var infoMessage = new StatusLinkMessage
                 {
                     Type = StatusLinkMessageType.GetPanelInfo
                 };
 
-                iface.Pipes[0x2].Write(infoMessage.ToBuffer());
+                using UsbEndpointWriter writer = usbDevice.OpenEndpointWriter(WriteEndpointID.Ep02);
+                writer.Write(infoMessage.ToBuffer(), 2000, out int _);
+
+                //iface.Pipes[0x2].Write(infoMessage.ToBuffer());
                 Trace.WriteLine("Sent infoTag");
 
                 byte[] responseBuffer = new byte[100]; // 20-byte header + 80-byte payload
-                int bytesRead = iface.Pipes[0x82].Read(responseBuffer);
+
+                using UsbEndpointReader reader = usbDevice.OpenEndpointReader(ReadEndpointID.Ep02);
+                reader.Read(responseBuffer, 2000, out int bytesRead);
+
+                //int bytesRead = iface.Pipes[0x82].Read(responseBuffer);
 
                 var panelInfo = BeadaPanelParser.ParsePanelInfoResponse(responseBuffer);
 
@@ -139,7 +167,9 @@ namespace InfoPanel
                 };
 
                 //always reset first
-                iface.Pipes[0x2].Write(resetTag.ToBuffer());
+                //iface.Pipes[0x2].Write(resetTag.ToBuffer());
+
+                writer.Write(resetTag.ToBuffer(), 2000, out int _);
 
                 //wait for reset
                 await Task.Delay(1000, token);
@@ -148,10 +178,16 @@ namespace InfoPanel
                 var brightness = ConfigModel.Instance.Settings.BeadaPanelBrightness;
 
                 brightnessTag.Payload = panelInfo.PanelLinkVersion == 1 ? [(byte)((brightness / 100.0 * 75) + 25)] : [(byte)brightness];
-                iface.Pipes[0x2].Write(brightnessTag.ToBuffer());
+                //iface.Pipes[0x2].Write(brightnessTag.ToBuffer());
+
+                writer.Write(brightnessTag.ToBuffer(), 2000, out int _);
 
                 //start stream
-                iface.Pipes[0x1].Write(startTag.ToBuffer());
+                //iface.Pipes[0x1].Write(startTag.ToBuffer());
+
+                using UsbEndpointWriter dataWriter = usbDevice.OpenEndpointWriter(WriteEndpointID.Ep01);
+                dataWriter.Write(startTag.ToBuffer(), 2000, out int _);
+
                 Trace.WriteLine("Sent startTag");
 
                 try
@@ -159,34 +195,109 @@ namespace InfoPanel
                     var fpsCounter = new FpsCounter();
                     var stopwatch = new Stopwatch();
 
-                    while (!token.IsCancellationRequested)
+                    var queue = new ConcurrentQueue<byte[]>();
+
+                    var renderTask = Task.Run(async () =>
                     {
-                        stopwatch.Restart();
-
-                        if (brightness != ConfigModel.Instance.Settings.BeadaPanelBrightness)
+                        var stopwatch1 = new Stopwatch();
+                        while (!token.IsCancellationRequested)
                         {
-                            brightness = ConfigModel.Instance.Settings.BeadaPanelBrightness;
-                            brightnessTag.Payload = panelInfo.PanelLinkVersion == 1 ? [(byte)((brightness / 100.0 * 75) + 25)] : [(byte)brightness];
-                            iface.Pipes[0x2].Write(brightnessTag.ToBuffer());
-                        }
+                            stopwatch1.Restart();
+                            var frame = GenerateLcdBuffer();
+                            //Trace.WriteLine($"GenerateBuffer {stopwatch1.ElapsedMilliseconds}ms");
+                            if (frame != null)
+                                queue.Enqueue(frame);
 
-                        if (GenerateLcdBuffer() is byte[] buffer)
+                            // Remove oldest if over capacity
+                            while (queue.Count > 2)
+                            {
+                                queue.TryDequeue(out _);
+                            }
+
+                            var targetFrameTime = 1000.0 / ConfigModel.Instance.Settings.TargetFrameRate;
+                            if (stopwatch1.ElapsedMilliseconds < targetFrameTime)
+                            {
+                                var sleep = (int)(targetFrameTime - stopwatch1.ElapsedMilliseconds);
+                                //Trace.WriteLine($"Sleep {sleep}ms");
+                                await Task.Delay(sleep, token);
+                            }
+                        }
+                    }, token);
+
+                    var sendTask = Task.Run(async () =>
+                    {
+                        var stopwatch2 = new Stopwatch();
+                        while (!token.IsCancellationRequested)
                         {
-                            iface.Pipes[0x1].Write(buffer);
-                            fpsCounter.Update();
-                        }
+                            if (brightness != ConfigModel.Instance.Settings.BeadaPanelBrightness)
+                            {
+                                brightness = ConfigModel.Instance.Settings.BeadaPanelBrightness;
+                                brightnessTag.Payload = panelInfo.PanelLinkVersion == 1 ? [(byte)((brightness / 100.0 * 75) + 25)] : [(byte)brightness];
+                                //iface.Pipes[0x2].Write(brightnessTag.ToBuffer());
+                                writer.Write(brightnessTag.ToBuffer(), 2000, out int _);
+                            }
 
-                        SharedModel.Instance.BeadaPanelFrameRate = fpsCounter.FramesPerSecond;
-                        SharedModel.Instance.BeadaPanelFrameTime = stopwatch.ElapsedMilliseconds;
+                            if (queue.TryDequeue(out var frame))
+                            {
+                                stopwatch2.Restart();
+                                dataWriter.Write(frame, 2000, out int _);
+                                Trace.WriteLine($"Post render: {stopwatch2.ElapsedMilliseconds}ms.");
+                                fpsCounter.Update();
+                                Trace.WriteLine($"FPS: {fpsCounter.FramesPerSecond}");
 
-                        var targetFrameTime = 1000.0 / ConfigModel.Instance.Settings.TargetFrameRate;
-                        if (stopwatch.ElapsedMilliseconds < targetFrameTime)
-                        {
-                            var sleep = (int)(targetFrameTime - stopwatch.ElapsedMilliseconds);
-                            //Trace.WriteLine($"Sleep {sleep}ms");
-                            await Task.Delay(sleep, token);
+                                SharedModel.Instance.BeadaPanelFrameRate = fpsCounter.FramesPerSecond;
+                                SharedModel.Instance.BeadaPanelFrameTime = stopwatch2.ElapsedMilliseconds;
+
+                                var targetFrameTime = 1000.0 / ConfigModel.Instance.Settings.TargetFrameRate;
+                                if (stopwatch2.ElapsedMilliseconds < targetFrameTime)
+                                {
+                                    var sleep = (int)(targetFrameTime - stopwatch2.ElapsedMilliseconds);
+                                    Trace.WriteLine($"Sleep {sleep}ms");
+                                    await Task.Delay(sleep, token);
+                                }
+                            }
+                            else
+                            {
+                                await Task.Delay(1);
+                            }
                         }
-                    }
+                    }, token);
+
+                    await Task.WhenAll(renderTask, sendTask);
+
+                    //while (!token.IsCancellationRequested)
+                    //{
+                    //    stopwatch.Restart();
+
+                    //    if (brightness != ConfigModel.Instance.Settings.BeadaPanelBrightness)
+                    //    {
+                    //        brightness = ConfigModel.Instance.Settings.BeadaPanelBrightness;
+                    //        brightnessTag.Payload = panelInfo.PanelLinkVersion == 1 ? [(byte)((brightness / 100.0 * 75) + 25)] : [(byte)brightness];
+                    //        //iface.Pipes[0x2].Write(brightnessTag.ToBuffer());
+                    //        writer.Write(brightnessTag.ToBuffer(), 2000, out int _);
+                    //    }
+
+                    //    if (GenerateLcdBuffer() is byte[] buffer)
+                    //    {
+                    //        Trace.WriteLine($"Render: {stopwatch.ElapsedMilliseconds}ms.");
+                    //        //iface.Pipes[0x1].Write(buffer);
+                    //        dataWriter.Write(buffer, 2000, out int _);
+                    //        Trace.WriteLine($"Post render: {stopwatch.ElapsedMilliseconds}ms.");
+                    //        fpsCounter.Update();
+                    //    }
+
+                    //    SharedModel.Instance.BeadaPanelFrameRate = fpsCounter.FramesPerSecond;
+                    //    SharedModel.Instance.BeadaPanelFrameTime = stopwatch.ElapsedMilliseconds;
+
+
+                    //    var targetFrameTime = 1000.0 / ConfigModel.Instance.Settings.TargetFrameRate;
+                    //    if (stopwatch.ElapsedMilliseconds < targetFrameTime)
+                    //    {
+                    //        var sleep = (int)(targetFrameTime - stopwatch.ElapsedMilliseconds);
+                    //        Trace.WriteLine($"Sleep {sleep}ms");
+                    //        await Task.Delay(sleep, token);
+                    //    }
+                    //}
                 }
                 catch (TaskCanceledException)
                 {
@@ -203,21 +314,24 @@ namespace InfoPanel
                         if (_shutdown)
                         {
                             //end stream
-                            iface.Pipes[0x1].Write(endTag.ToBuffer());
+                            //iface.Pipes[0x1].Write(endTag.ToBuffer());
                             Trace.WriteLine("Sent endTag");
 
                             brightnessTag.Payload = [0];
-                            iface.Pipes[0x2].Write(brightnessTag.ToBuffer());
+                            //iface.Pipes[0x2].Write(brightnessTag.ToBuffer());
                         }
                         else
                         {
                             //draw splash
-                            using var bitmap = PanelDrawTask.RenderSplash(_panelWidth, _panelHeight,
+                            using var bitmap = PanelDrawTask.RenderSplash(_panelWidth, _panelHeight, pixelFormat: PixelFormat.Format16bppRgb565,
                             rotateFlipType: (RotateFlipType)Enum.ToObject(typeof(RotateFlipType), ConfigModel.Instance.Settings.BeadaPanelRotation));
-                            iface.Pipes[0x1].Write(BitmapToRgb16(bitmap));
+                            //iface.Pipes[0x1].Write(BitmapToRgb16(bitmap));
+
+                            dataWriter.Write(BitmapToRgb16(bitmap), 2000, out int _);
 
                             //end stream
-                            iface.Pipes[0x1].Write(endTag.ToBuffer());
+                            //iface.Pipes[0x1].Write(endTag.ToBuffer());
+                            dataWriter.Write(endTag.ToBuffer(), 2000, out int _);
                             Trace.WriteLine("Sent endTag");
                         }
                     }
---
+++ InfoPanel/Services/TuringPanelTask.cs
@@ -0,0 +1,230 @@
+﻿using InfoPanel.Extensions;
+using InfoPanel.Models;
+using InfoPanel.TuringPanel;
+using InfoPanel.Utils;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InfoPanel
+{
+    public sealed class TuringPanelTask : BackgroundTask
+    {
+        private static readonly Lazy<TuringPanelTask> _instance = new(() => new TuringPanelTask());
+
+        private volatile int _panelWidth = 480;
+        private volatile int _panelHeight = 1920;
+
+        public static TuringPanelTask Instance => _instance.Value;
+
+        private TuringPanelTask() { }
+
+
+        public byte[]? GenerateLcdBuffer()
+        {
+            var profileGuid = ConfigModel.Instance.Settings.TuringPanelProfile;
+
+            if (ConfigModel.Instance.GetProfile(profileGuid) is Profile profile)
+            {
+                using var bitmap = PanelDrawTask.Render(profile, false, videoBackgroundFallback: true, pixelFormat: PixelFormat.Format16bppRgb565, overrideDpi: true);
+                var rotation = ConfigModel.Instance.Settings.TuringPanelRotation;
+                if (rotation != ViewModels.LCD_ROTATION.RotateNone)
+                {
+                    var rotateFlipType = (RotateFlipType)Enum.ToObject(typeof(RotateFlipType), rotation);
+                    bitmap.RotateFlip(rotateFlipType);
+                }
+
+                using var resizedBitmap = (_panelWidth == 0 || _panelHeight == 0)
+                   ? BitmapExtensions.EnsureBitmapSize(bitmap, bitmap.Width, bitmap.Height)
+                   : BitmapExtensions.EnsureBitmapSize(bitmap, _panelWidth, _panelHeight);
+
+                return BitmapToPngBytes(resizedBitmap);
+            }
+
+            return null;
+        }
+
+        public static byte[] BitmapToPngBytes(Bitmap bitmap)
+        {
+            //var sw = new Stopwatch();
+            //sw.Start();
+            using var ms = new MemoryStream();
+            bitmap.Save(ms, ImageFormat.Png);
+            //Trace.WriteLine($"BitmapToPngBytes: {sw.ElapsedMilliseconds}ms");
+            return ms.ToArray();
+        }
+
+        protected override async Task DoWorkAsync(CancellationToken token)
+        {
+            await Task.Delay(300, token);
+            try
+            {
+                using var device = new TuringDevice();
+
+                if (!device.Initialize())
+                {
+                    Trace.WriteLine("Failed to initialize the device.");
+                    return;
+                }
+
+                SharedModel.Instance.TuringPanelRunning = true;
+
+                try
+                {
+                    device.DelaySync();
+                    device.DelaySync();
+
+                    //set brightness
+                    var brightness = ConfigModel.Instance.Settings.TuringPanelBrightness;
+                    device.SendBrightnessCommand((byte) brightness);
+
+                    device.DelaySync();
+
+                    var fpsCounter = new FpsCounter();
+                    var stopwatch = new Stopwatch();
+
+                    var queue = new ConcurrentQueue<byte[]>();
+
+                    var renderTask = Task.Run(async () =>
+                    {
+                        var stopwatch1 = new Stopwatch();
+                        while (!token.IsCancellationRequested)
+                        {
+                            stopwatch1.Restart();
+                            var frame = GenerateLcdBuffer();
+                            //Trace.WriteLine($"GenerateBuffer {stopwatch1.ElapsedMilliseconds}ms");
+                            if (frame != null)
+                                queue.Enqueue(frame);
+
+                            // Remove oldest if over capacity
+                            while (queue.Count > 2)
+                            {
+                                queue.TryDequeue(out _);
+                            }
+
+                            var targetFrameTime = 1000.0 / ConfigModel.Instance.Settings.TargetFrameRate;
+                            if (stopwatch1.ElapsedMilliseconds < targetFrameTime)
+                            {
+                                var sleep = (int)(targetFrameTime - stopwatch1.ElapsedMilliseconds);
+                                Trace.WriteLine($"Sleep {sleep}ms");
+                                await Task.Delay(sleep, token);
+                            }
+                        }
+                    }, token);
+
+                    var sendTask = Task.Run(async () =>
+                    {
+                        var stopwatch2 = new Stopwatch();
+                        while (!token.IsCancellationRequested)
+                        {
+                            if (brightness != ConfigModel.Instance.Settings.TuringPanelBrightness)
+                            {
+                                brightness = ConfigModel.Instance.Settings.TuringPanelBrightness;
+                                device.SendBrightnessCommand((byte)brightness);
+                                device.DelaySync();
+                            }
+
+                            if (queue.TryDequeue(out var frame))
+                            {
+                                stopwatch2.Restart();
+                                SendPngBytes(device, frame);
+                                //Trace.WriteLine($"Post render: {stopwatch2.ElapsedMilliseconds}ms.");
+                                device.ReadFlush();
+                                fpsCounter.Update();
+
+                                //Trace.WriteLine($"FPS: {fpsCounter.FramesPerSecond}");
+
+                                SharedModel.Instance.TuringPanelFrameRate = fpsCounter.FramesPerSecond;
+                                SharedModel.Instance.TuringPanelFrameTime = stopwatch2.ElapsedMilliseconds;
+
+                                var targetFrameTime = 1000.0 / ConfigModel.Instance.Settings.TargetFrameRate;
+                                if (stopwatch2.ElapsedMilliseconds < targetFrameTime)
+                                {
+                                    var sleep = (int)(targetFrameTime - stopwatch2.ElapsedMilliseconds);
+                                    //Trace.WriteLine($"Sleep {sleep}ms");
+                                    await Task.Delay(sleep, token);
+                                }
+                            }
+                            else
+                            {
+                                await Task.Delay(1);
+                            }
+                        }
+                    }, token);
+
+
+                    await Task.WhenAll(renderTask, sendTask);
+                }
+                catch (TaskCanceledException)
+                {
+                    Trace.WriteLine("Task cancelled");
+                }
+                catch (Exception ex)
+                {
+                    Trace.WriteLine($"Exception during work: {ex.Message}");
+                }
+                finally
+                {
+                    try
+                    {
+                        if (_shutdown)
+                        {
+                            device.SendBrightnessCommand(0);
+                        }
+                        else
+                        {
+                            //draw splash
+                            using var bitmap = PanelDrawTask.RenderSplash(_panelWidth, _panelHeight, pixelFormat: PixelFormat.Format16bppRgb565,
+                            rotateFlipType: (RotateFlipType)Enum.ToObject(typeof(RotateFlipType), ConfigModel.Instance.Settings.TuringPanelRotation));
+                            SendPngBytes(device, BitmapToPngBytes(bitmap));
+                            device.ReadFlush();
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine($"Exception when sending ResetTag: {ex.Message}");
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine("BeadaPanel: Init error");
+            }
+            finally
+            {
+                SharedModel.Instance.TuringPanelRunning = false;
+            }
+        }
+
+        private static bool SendPngBytes(TuringDevice device, byte[] pngData)
+        {
+            int imgSize = pngData.Length;
+            byte[] cmdPacket = device.BuildCommandPacketHeader(102);
+
+            // Set image size in the packet (big-endian)
+            cmdPacket[8] = (byte)((imgSize >> 24) & 0xFF);
+            cmdPacket[9] = (byte)((imgSize >> 16) & 0xFF);
+            cmdPacket[10] = (byte)((imgSize >> 8) & 0xFF);
+            cmdPacket[11] = (byte)(imgSize & 0xFF);
+
+            // Encrypt the command packet
+            byte[] encryptedPacket = device.EncryptCommandPacket(cmdPacket);
+
+            // Combine the encrypted packet with the image data
+            byte[] fullPayload = new byte[encryptedPacket.Length + pngData.Length];
+            Buffer.BlockCopy(encryptedPacket, 0, fullPayload, 0, encryptedPacket.Length);
+            Buffer.BlockCopy(pngData, 0, fullPayload, encryptedPacket.Length, pngData.Length);
+
+            // Write the payload to the device
+            return device.WriteToDevice(fullPayload);
+        }
+    }
+
+
+}
